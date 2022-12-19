1ba4eac4a7552d6c61fcfd584a2f96ddf5d184f4
madhu chandru <madhuchandru2000@gmail.com>
2022-12-19 23:16:46 +0530

    constrains

---
+++ hotel.sql
@@ -0,0 +1,13 @@
+use xworkz_1;
+create table hotel(SLNO int(40),hotelName varchar(40)unique,ownername varchar(40) not null, typeofhotel varchar(40),contactnumber bigint(40),Location varchar(40),famousfood varchar(40),noofvarietys int(40),numberofworks varchar(40),ratings int(40),check(ratings<=5));
+select* from hotel;
+INSERT INTO  hotel values(1,'mtr','madan','veg',7975435531,'bengalore','dosa',32,'340',5);
+INSERT INTO hotel values(2,'udupiathitya','vinayaka','veg',8546512475,'udupi','karabath',24,'120',4);
+INSERT INTO hotel values(3,'menakshibavan','menakshi','veg',8451245573,'shivamogga','paddu',26,'125',5);
+INSERT INTO hotel values(4,'kalpathura','kalpathura','veg',7542154435,'tiptur','mells',40,'128',4);
+INSERT INTO hotel values(5,'mtr','vasu','veg',5421468575,'tarikere','hedlli',21,'160',3);
+INSERT INTO hotel  values(6, 'madhu', 'trupthi','veg',2145765543,'tharekere','hedlli',19,'140',4);
+INSERT INTO hotel values(7,'Namaste','narayana','veg',547564145,'bengalore','kesaribath',54,'50',5);
+INSERT INTO hotel  values(8, 'madurihedllishop', 'sanmugam','veg',5421154757,'maduri','hedlli',54,'200',4);
+INSERT INTO hotel  values(9, 'subam', 'sampath','veg',8454752145,'shivamogga','noon',90,'140',5);
+INSERT INTO hotel  values(10, 'ctr', 'sumanth','veg',2545621475,'begalore','dosa',70,'200',6);
