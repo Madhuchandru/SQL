1aafab8c55f1d2c36b48d65adb3e0ab1e7b9ec30
madhu chandru <madhuchandru2000@gmail.com>
2022-12-21 01:23:43 +0530

    constrains

--- hotel.sql
+++ hotel.sql
@@ -11,3 +11,31 @@ INSERT INTO hotel values(7,'Namaste','narayana','veg',547564145,'bengalore','kes
 INSERT INTO hotel  values(8, 'madurihedllishop', 'sanmugam','veg',5421154757,'maduri','hedlli',54,'200',4);
 INSERT INTO hotel  values(9, 'subam', 'sampath','veg',8454752145,'shivamogga','noon',90,'140',5);
 INSERT INTO hotel  values(10, 'ctr', 'sumanth','veg',2545621475,'begalore','dosa',70,'200',6);
+
+
+create table apartments(id int primary key ,a_name varchar(30),location varchar(30) not null,no_of_rooms int not null);
+select *from apartments;
+Insert into apartments values(1,'prestige','begalore',2);
+insert into apartments values(1,'greenvali','shivamogga',3);
+insert into apartments values (2,'egale','thumkure',1);
+insert into apartments values(3,'godrage','bengalore',3);
+
+create table restorent(id int,r_name varchar(30),location varchar(30) not null , no_of_workrs int not null, ratings int not null, primary key(id,r_name));
+select * from restorent;
+insert into restorent values (1,'mtr','bengalore',40,5);
+insert into restorent values (1,'ctr','bengalore',30,4);
+insert into restorent values (2,'gokul','shivamogga',20,3);
+
+create table bank_details(id int not null ,b_name varchar(30),b_id int primary key,b_location varchar(30),no_of_customers int not null);
+select *from bank_details;
+insert into bank_details values (1,'canara',577,'bengalore',400);
+insert into bank_details values (2,'axis',544,'shivamogga',500);
+insert into bank_details values(3,'syndicate',433,'tharikere',200);
+
+
+create table cust_details (id int not null,c_name varchar(30),b_id int ,balance bigint not null,C_location varchar(30), foreign key (b_id) references bank_details(b_id));
+ insert into cust_details  values (1,'mohan',544,5486,'mysore');
+ select * from cust_details;
+ insert into cust_details values (2,'ragu',433,54000,'shivamogga');
+ insert into cust_details values (3,'sumanth',477,45000,'begalore');
+ 
